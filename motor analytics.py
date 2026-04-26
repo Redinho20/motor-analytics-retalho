ad49ca166bb1e01c779cc6c1c162debe02bcdea1
REDINHO MARQUES <MARQUESREDINHO@GMAIL.COM>
2026-04-26 22:41:18 +0100

    query 6,7,8,9,10,11,12

--- motor analytics.py
+++ motor analytics.py
@@ -88,6 +88,13 @@ def contar_entradas_por_zona_intervalo(eventos, inicio, fim):
 
     return contagens
 
+def timestamp_para_segundos(timestamp):
+    hora = int(timestamp[11:13])
+    minuto = int(timestamp[14:16])
+    segundo = int(timestamp[17:19])
+
+    return hora * 3600 + minuto * 60 + segundo
+
 
 class MaxHeap:
     def __init__(self):
@@ -140,7 +147,8 @@ class MaxHeap:
                    self.dados[maior]=temp
                    indice=maior
               else:
-                   break  
+                   break
+      
 # =========================
 # QUERIES
 # =========================
@@ -352,6 +360,379 @@ def query_comparacao_entre_dias(eventos, dia1, dia2):
     print("Média permanência:", round(media2, 2), "segundos")
     print("Zona mais visitada:", zona2[0], "->", zona2[1])
 
+def query_top_k_periodos_afluencia(eventos, inicio, fim, k, zona_filtro):
+    blocos = []
+
+    for evento in eventos:
+        if inicio <= evento.timestamp <= fim:
+            if zona_filtro == "" or evento.zone_id == zona_filtro:
+                hora = int(evento.timestamp[11:13])
+                minuto = int(evento.timestamp[14:16])
+
+                if minuto < 30:
+                    bloco_inicio = evento.timestamp[0:11] + str(hora).zfill(2) + ":00:00"
+                    bloco_fim = evento.timestamp[0:11] + str(hora).zfill(2) + ":29:59"
+                else:
+                    bloco_inicio = evento.timestamp[0:11] + str(hora).zfill(2) + ":30:00"
+                    bloco_fim = evento.timestamp[0:11] + str(hora).zfill(2) + ":59:59"
+
+                nome_bloco = bloco_inicio + " até " + bloco_fim
+                encontrado = False
+
+                for i in range(len(blocos)):
+                    if blocos[i][0] == nome_bloco:
+                        blocos[i][1] += 1
+                        encontrado = True
+                        break
+
+                if not encontrado:
+                    blocos.append([nome_bloco, 1])
+
+    heap = MaxHeap()
+
+    for item in blocos:
+        heap.insert(item)
+
+    print(f"\nTOP {k} PERÍODOS DE MAIOR AFLUÊNCIA")
+
+    for i in range(k):
+        maior = heap.remove_max()
+        if maior is not None:
+            print(maior[0], "->", maior[1], "eventos")
+
+def query_zonas_maior_permanencia(eventos, k, genero_filtro, idade_filtro):
+    zonas = []
+
+    for evento in eventos:
+        if evento.event_type == "linger":
+            if genero_filtro == "" or evento.gender == genero_filtro:
+                if idade_filtro == "" or evento.age_range == idade_filtro:
+                    zona = evento.zone_id
+                    duracao = int(evento.duration_s)
+                    encontrado = False
+
+                    for i in range(len(zonas)):
+                        if zonas[i][0] == zona:
+                            zonas[i][1] += duracao
+                            zonas[i][2] += 1
+                            encontrado = True
+                            break
+
+                    if not encontrado:
+                        zonas.append([zona, duracao, 1])
+
+    heap = MaxHeap()
+
+    for item in zonas:
+        zona = item[0]
+        soma = item[1]
+        quantidade = item[2]
+
+        media = soma / quantidade
+        heap.insert([zona, media])
+
+    print(f"\nTOP {k} ZONAS COM MAIOR TEMPO MÉDIO DE PERMANÊNCIA")
+
+    for i in range(k):
+        maior = heap.remove_max()
+        if maior is not None:
+            print(maior[0], "->", round(maior[1], 2), "segundos")
+#======12=======
+def query_perfil_demografico(eventos, zona):
+    generos = []
+    idades = []
+
+    for evento in eventos:
+        if evento.zone_id == zona and evento.event_type == "entry":
+
+            # género
+            encontrado = False
+            for i in range(len(generos)):
+                if generos[i][0] == evento.gender:
+                    generos[i][1] += 1
+                    encontrado = True
+                    break
+            if not encontrado:
+                generos.append([evento.gender, 1])
+
+            # idade
+            encontrado = False
+            for i in range(len(idades)):
+                if idades[i][0] == evento.age_range:
+                    idades[i][1] += 1
+                    encontrado = True
+                    break
+            if not encontrado:
+                idades.append([evento.age_range, 1])
+
+    print(f"\nPERFIL DEMOGRÁFICO - {zona}")
+
+    print("\nPor género:")
+    for g in generos:
+        print(g[0], "->", g[1])
+
+    print("\nPor faixa etária:")
+    for i in idades:
+        print(i[0], "->", i[1])
+#Fim12
+
+#10
+def raiz_quadrada(valor):
+    if valor == 0:
+        return 0
+
+    tentativa = valor
+
+    for i in range(20):
+        tentativa = (tentativa + valor / tentativa) / 2
+
+    return tentativa
+
+
+def query_anomalias(eventos):
+    dados = []
+
+    for evento in eventos:
+        if evento.event_type == "entry":
+            zona = evento.zone_id
+            dia = evento.timestamp[0:10]
+            hora = evento.timestamp[11:13] + ":00-" + str(int(evento.timestamp[11:13]) + 1) + ":00"
+            bloco = dia + " " + hora
+
+            encontrado = False
+
+            for i in range(len(dados)):
+                if dados[i][0] == zona and dados[i][1] == bloco:
+                    dados[i][2] += 1
+                    encontrado = True
+                    break
+
+            if not encontrado:
+                dados.append([zona, bloco, 1])
+
+    zonas = []
+
+    for item in dados:
+        zona = item[0]
+
+        if zona not in zonas:
+            zonas.append(zona)
+
+    print("\nANOMALIAS POR ZONA")
+
+    for zona in zonas:
+        valores = []
+
+        for item in dados:
+            if item[0] == zona:
+                valores.append(item[2])
+
+        soma = 0
+        for valor in valores:
+            soma += valor
+
+        media = soma / len(valores)
+
+        soma_desvios = 0
+        for valor in valores:
+            diferenca = valor - media
+            soma_desvios += diferenca * diferenca
+
+        variancia = soma_desvios / len(valores)
+        desvio = raiz_quadrada(variancia)
+
+        print("\nZona:", zona)
+        encontrou = False
+
+        for item in dados:
+            if item[0] == zona:
+                contagem = item[2]
+
+                if contagem > media + 2 * desvio or contagem < media - 2 * desvio:
+                    print(item[1], "->", contagem, "entradas")
+                    encontrou = True
+
+        if not encontrou:
+            print("Sem anomalias")
+#11
+def query_composta(eventos, inicio, fim, zona_filtro, genero_filtro, idade_filtro):
+    total_eventos = 0
+    soma_permanencia = 0
+    quantidade_linger = 0
+
+    distribuicao_horas = []
+
+    for h in range(9, 21):
+        distribuicao_horas.append([h, 0])
+
+    for evento in eventos:
+        if inicio <= evento.timestamp <= fim:
+
+            if zona_filtro == "" or evento.zone_id == zona_filtro:
+                if genero_filtro == "" or evento.gender == genero_filtro:
+                    if idade_filtro == "" or evento.age_range == idade_filtro:
+
+                        total_eventos += 1
+
+                        hora = int(evento.timestamp[11:13])
+                        if hora >= 9 and hora < 21:
+                            indice = hora - 9
+                            distribuicao_horas[indice][1] += 1
+
+                        if evento.event_type == "linger":
+                            soma_permanencia += int(evento.duration_s)
+                            quantidade_linger += 1
+
+    if quantidade_linger > 0:
+        media = soma_permanencia / quantidade_linger
+    else:
+        media = 0
+
+    print("\nQUERY COMPOSTA")
+    print("Total de eventos:", total_eventos)
+    print("Tempo médio de permanência:", round(media, 2), "segundos")
+
+    print("\nDistribuição por hora:")
+    for item in distribuicao_horas:
+        print(str(item[0]) + ":00-" + str(item[0] + 1) + ":00", "->", item[1], "eventos")
+
+def query_fluxo_entre_zonas(eventos, threshold, n):
+    transicoes = []
+
+    for i in range(len(eventos)):
+        evento_saida = eventos[i]
+
+        if evento_saida.event_type == "exit":
+            for j in range(i + 1, len(eventos)):
+                evento_entrada = eventos[j]
+
+                if evento_entrada.timestamp[0:10] != evento_saida.timestamp[0:10]:
+                    break
+
+                if evento_entrada.event_type == "entry":
+                    mesma_pessoa = (
+                        evento_saida.gender == evento_entrada.gender and
+                        evento_saida.age_range == evento_entrada.age_range
+                    )
+
+                    if mesma_pessoa:
+                        tempo_saida = timestamp_para_segundos(evento_saida.timestamp)
+                        tempo_entrada = timestamp_para_segundos(evento_entrada.timestamp)
+                        diferenca = tempo_entrada - tempo_saida
+
+                        if diferenca >= 0 and diferenca <= threshold:
+                            origem = evento_saida.zone_id
+                            destino = evento_entrada.zone_id
+
+                            if origem != destino:
+                                encontrado = False
+
+                                for k in range(len(transicoes)):
+                                    if transicoes[k][0] == origem and transicoes[k][1] == destino:
+                                        transicoes[k][2] += 1
+                                        encontrado = True
+                                        break
+
+                                if not encontrado:
+                                    transicoes.append([origem, destino, 1])
+
+                            break
+
+    heap = MaxHeap()
+
+    for item in transicoes:
+        heap.insert([[item[0], item[1]], item[2]])
+
+    print(f"\nTOP {n} TRANSIÇÕES ENTRE ZONAS")
+
+    for i in range(n):
+        maior = heap.remove_max()
+        if maior is not None:
+            origem = maior[0][0]
+            destino = maior[0][1]
+            quantidade = maior[1]
+            print(origem, "->", destino, ":", quantidade)
+#8
+def construir_sequencia_zonas(eventos):
+    entradas = []
+
+    for evento in eventos:
+        if evento.event_type == "entry":
+            entradas.append([evento.timestamp, evento.zone_id])
+
+    entradas = merge_sort_eventos_temporais(entradas)
+
+    sequencia = ""
+
+    for i in range(len(entradas)):
+        sequencia += entradas[i][1]
+
+        if i < len(entradas) - 1:
+            sequencia += "-"
+
+    return sequencia
+
+
+def calcular_prefixos_kmp(padrao):
+    prefixos = []
+    for i in range(len(padrao)):
+        prefixos.append(0)
+
+    tamanho = 0
+    i = 1
+
+    while i < len(padrao):
+        if padrao[i] == padrao[tamanho]:
+            tamanho += 1
+            prefixos[i] = tamanho
+            i += 1
+        else:
+            if tamanho != 0:
+                tamanho = prefixos[tamanho - 1]
+            else:
+                prefixos[i] = 0
+                i += 1
+
+    return prefixos
+
+
+def kmp_procurar(texto, padrao):
+    if padrao == "":
+        return 0
+
+    prefixos = calcular_prefixos_kmp(padrao)
+
+    i = 0
+    j = 0
+    ocorrencias = 0
+
+    while i < len(texto):
+        if texto[i] == padrao[j]:
+            i += 1
+            j += 1
+
+        if j == len(padrao):
+            ocorrencias += 1
+            j = prefixos[j - 1]
+
+        elif i < len(texto) and texto[i] != padrao[j]:
+            if j != 0:
+                j = prefixos[j - 1]
+            else:
+                i += 1
+
+    return ocorrencias
+
+
+def query_pesquisa_sequencia(eventos, padrao):
+    sequencia = construir_sequencia_zonas(eventos)
+
+    ocorrencias = kmp_procurar(sequencia, padrao)
+
+    print("\nPESQUISA DE SEQUÊNCIA DE ZONAS")
+    print("Padrão:", padrao)
+    print("Ocorrências encontradas:", ocorrencias)
+
 lista_zonas = ["Z_E1", "Z_E2", "Z_X1", "Z_CK", "Z_C1", "Z_C2", "Z_C3",
                "Z_S1", "Z_S2", "Z_S3", "Z_S4", "Z_S5", "Z_S6", "Z_S7",
                "Z_N1", "Z_N2", "Z_N3", "Z_N4", "Z_N5", "Z_N6", "Z_N7",
@@ -368,6 +749,13 @@ def menu(eventos,lista_zonas):
         print("4 - Picos de ocupação")
         print("5 - Top-K zonas num intervalo")
         print("6 - Comparação entre dias")
+        print("7 - Top-K períodos de maior afluência")
+        print("8 - Zonas com maior tempo médio de permanência")
+        print("9 - Perfil demográfico por zona")
+        print("10 - Deteção de anomalias")
+        print("11 - Query composta")
+        print("12 - Fluxo entre zonas")
+        print("13 - Pesquisa de sequência de zonas")
         print("0 - Sair")
 
         opcao = input("Escolha uma opção: ")
@@ -413,9 +801,59 @@ def menu(eventos,lista_zonas):
             dia2 = input("Digite o segundo dia (YYYY-MM-DD): ")
             query_comparacao_entre_dias(eventos, dia1, dia2)
 
+        elif opcao == "7":
+            inicio = input("Data/hora início (YYYY-MM-DD HH:MM:SS): ")
+            fim = input("Data/hora fim (YYYY-MM-DD HH:MM:SS): ")
+            zona = input("Zona para filtrar (enter para todas): ")
+
+            try:
+                k = int(input("Quantos períodos quer ver? "))
+            except:
+                print("Valor inválido")
+                continue
+
+            query_top_k_periodos_afluencia(eventos, inicio, fim, k, zona)
+
+        elif opcao == "8":
+            genero = input("Filtrar género (M/F ou enter para todos): ")
+            idade = input("Filtrar faixa etária (ex: adult ou enter para todas): ")
+
+            try:
+                k = int(input("Quantas zonas quer ver? "))
+            except:
+                print("Valor inválido")
+                continue
+
+            query_zonas_maior_permanencia(eventos, k, genero, idade)
+
+        elif opcao == "9":
+            zona = input("Digite a zona: ")
+            query_perfil_demografico(eventos, zona)
+
+        elif opcao == "10":
+            query_anomalias(eventos)
+
+        elif opcao == "11":
+            inicio = input("Data/hora início (YYYY-MM-DD HH:MM:SS): ")
+            fim = input("Data/hora fim (YYYY-MM-DD HH:MM:SS): ")
+            zona = input("Zona (enter para todas): ")
+            genero = input("Género M/F (enter para todos): ")
+            idade = input("Faixa etária (enter para todas): ")
+
+            query_composta(eventos, inicio, fim, zona, genero, idade)  
+
+        elif opcao == "12":
+            threshold = int(input("Threshold em segundos: "))
+            n = int(input("Quantas transições quer ver? "))
+            query_fluxo_entre_zonas(eventos, threshold, n)
+
+        elif opcao == "13":
+            padrao = input("Digite a sequência (ex: Z_E1-Z_C1-Z_S3): ")
+            query_pesquisa_sequencia(eventos, padrao)          
+
         elif opcao == "0":
-             print("A sair...")
-             break
+            print("A sair...")
+            break
 
         else:
             print("Opção inválida.")
